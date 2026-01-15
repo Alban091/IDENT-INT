e2932d604955013398cf34eccefe6a41ffce479c
Alban091 <albanr04@gmail.com>
2026-01-15 17:39:56 +0100

    Ajout d'un autre css, et ajout d'information et explication sur le site au cas ou pour que ça puisse servir.

--- recognition/urls.py
+++ recognition/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', views.upload_photo, name='upload_photo'),
     path('preview/', views.preview, name='preview'),
+    path('about/', views.about, name='about'),
 # Route de test pour la page 500 (à supprimer en production)
     path('test-500/', lambda request: 1/0, name='test_500'),
 ]
